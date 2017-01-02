cdc99be217a31bcc4286641830825c8fed1d51d4
ravinayak <ravinayak19@gmail.com>
2017-01-01 21:28:41 -0800

    Updated code to include support for removing a node and refactored code

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -31,6 +31,13 @@ module LinkedList
       add_node_support(value)
     end
 
+    # Remove a node
+    # @param value [String]
+    # @return [Node]
+    #
+    def remove_node(value)
+      remove_node_support(value)
+    end
     # Print list value
     # @return [String]
     #
@@ -44,5 +51,12 @@ module LinkedList
     def traverse
       traverse_support
     end
+
+    # Code to create a list and add nodes to it
+    # Print list
+    #
+    # require '/Users/ravinayak/projects/data_structures/linked_list/linked_list'
+    # list = LinkedList::LinkedList.new
+    # [89, 90, 14, 13, 15, 80, 45, 55, 65].each { |val| list.add_node(val) }
   end
 end
