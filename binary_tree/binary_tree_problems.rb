5151c203a5dd40cd255e0ad4b3e42961ef753759
ravinayak <ravinayak19@gmail.com>
2017-01-04 21:20:25 -0800

    -> Calculating depth of a binary tree

--- binary_tree/binary_tree_problems.rb
+++ binary_tree/binary_tree_problems.rb
@@ -19,5 +19,23 @@ module BinaryTree
       return 0 if node.nil?
       size_support(node.left) + 1 + size_support(node.right)
     end
+
+    # Returns maximum depth of a binary tree
+    # @return [Integer]
+    #
+    def depth
+      depth_support(self.root)
+    end
+
+    # Finds the maximum depth of a tree
+    # @param node [Node]
+    # @return [Integer]
+    #
+    def depth_support(node)
+      return 0 if node.nil?
+      l_depth = depth_support(node.left)
+      r_depth = depth_support(node.right)
+      [l_depth, r_depth].max + 1
+    end
   end
 end
