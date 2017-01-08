be09bdfe9709fc2afcef54a05bf6433f4275ae12
ravinayak <ravinayak19@gmail.com>
2017-01-08 11:53:37 -0800

    -> Included new methods or hasPathSum and printing paths in a tree

--- binary_tree/binary_tree_problems.rb
+++ binary_tree/binary_tree_problems.rb
@@ -37,5 +37,55 @@ module BinaryTree
       r_depth = depth_support(node.right)
       [l_depth, r_depth].max + 1
     end
+
+    # Returns true if there exists a path in the tree from root to leaf which
+    # adds up to the given sum
+    # @param node [Node]
+    # @param sum [Integer]
+    # @return [TrueClass/FalseClass]
+    #
+    def has_path_sum(node, sum)
+      return false if node.nil?
+      sub_sum = sum - node.value
+      return true if sub_sum == 0 && node.left.nil? && node.right.nil?
+      has_path_sum(node.left, sub_sum) || has_path_sum(node.right, sub_sum)
+    end
+
+    # Prints all the paths from the root to the leaf node in a tree
+    # @return [NIL]
+    #
+    def print_paths
+      path, counter = [], 0
+      puts '************************ Tree Paths ***********************************'
+      puts
+      print_tree_paths(self.root, path, counter)
+      puts
+      puts '***********************************************************************'
+    end
+
+    # Helper method to prints paths in a tree
+    # @param node [Node]
+    # @param path [Array]
+    # @param counter [Integer]
+    # @return [NIL]
+    #
+    def print_tree_paths(node, path, counter)
+      return if node.nil?
+      path[counter] = node
+      counter += 1
+      return print_nodes(path, counter) if node.left.nil? && node.right.nil?
+      print_tree_paths(node.left, path, counter) unless node.left.nil?
+      print_tree_paths(node.right, path, counter) unless node.right.nil?
+    end
+
+    # Helper method to print the paths in an array
+    # @param path [Array]
+    # @param counter [Integer]
+    # @return [NIL]
+    #
+    def print_nodes(path, counter)
+      (0..(counter - 1)).each { |i| print path[i].value.to_s + '  ' }
+      puts
+    end
   end
 end
