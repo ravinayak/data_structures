dfddcb3ad7418504bf4a76ca1cf3e301bbff35c2
ravinayak <ravinayak19@gmail.com>
2018-04-05 18:45:50 -0700

    Included enlisting DP problems

---
+++ dynamic_programming/dynamic_programming_problems.rb
@@ -0,0 +1,55 @@
+# Used for namespacing
+#
+module DynamicProgramming
+  # Enlist the problems of Dynamic Programming to be solved
+  # and implemented
+  #
+  module DynamicProgrammingProblems
+    # Below is a list of DP problems to be practiced and coded
+    #
+    # 1.  Longest Common Subsequence
+    # 2.  Longest Palindrome Subsequence [ Only Practice - variant of 1 ]
+    # 3.  OBST
+    # 4.  Coin Change
+    # 5.  Matrix Chain Multiplication
+    # 6.  Longest Simple Path in a directed acyclic graph
+    # 7.  Edit Distance
+    # 8.  Single Source Shortest Paths
+    # 9.  Knapsack
+    # 10. Rod Cutting
+    # 11. Box Stacking
+    # 12. Burst Balloon
+    # 13. Count number of BST given pre-order sequence
+    # 14. Egg Dropping Problem
+    # 15. Longest Increasing Subsequence
+    # 16. Matrix Size Rectangle of all 1's
+    # 17. Matrix Sub Square with sides X
+    # 18. Matrix Sum Increasing Subsequence
+    # 19. Matrix Sum Rectangular Submatrix in Matrix DP
+    # 20. Numbers without consecutive 1's in binary representation
+    # 21. Optimal Game Strategy Pick from ends of array
+    # 22. Palindrome Partition DP
+    # 23. Regular Expression DP
+    # 24. Staircase Problem Fibonacci Series
+    # 25. String Interleaving
+    # 26. Subset sum
+    # 27. Text Justification
+    # 28. Total ways in Matrix DP
+    # 29. Weighted JOb Scheduling DP
+    # 30. Wildcard Matching
+    # 31. Word break DP
+    #
+    # Conceptual Practice
+    # 1.  Super Mario Bros
+    # 2.  Printing neatly
+    # 3.  Tetris
+    # 4.  Bitonic euclidean traveling-salesman problem
+    # 5.  Planning a company party
+    # 6.  Viterbi Algorithm
+    # 7.  Breaking a string
+    # 8.  Planning an investment strategy
+    # 9.  Inventory Planning
+    # 10. Signing free-agent baseball players
+    # 11. Playing guitar
+  end
+end
