88c349b13adeea878a2d6346f61a78de9e47e702
ravinayak <ravinayak19@gmail.com>
2018-05-27 20:28:18 -0700

    Implemented a file to track problems to implement and corrected a typo in DP

--- problems_to_solve.rb
+++ problems_to_solve.rb
@@ -4,3 +4,5 @@
 # 2.  Trim a BST (ensure that the parent node is set to NIL)
 # 3.  Implement Heap
 # 4.  Construct BST from given traversals
+# 5.  Given a target, find if a BST has two numbers which add upto target
+# 6.  Given a target, find if a BST has two numbers in subtree of a node which add upto a target
