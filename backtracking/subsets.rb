04d54bc281b80c98e04f7cb1e65b6170a1380248
ravinayak <ravinayak19@gmail.com>
2018-07-06 10:18:54 -0700

    Included implementation for subsets

---
+++ backtracking/subsets.rb
@@ -0,0 +1,41 @@
+# Used for namespacing
+#
+module Backtracking
+  # Class
+  #
+  class Subsets
+  
+    attr_accessor :arr
+    
+    def initialize(arr: [])
+      @arr = arr
+    end
+    
+    def generate_power_subsets(array)
+      prep_power_subsets(array)
+    end
+    
+    private
+    
+    def prep_power_subsets(array)
+      power_subset_arr = []
+      len = array.length
+      (0...(2 ** len)).each do |num|
+        tmp_arr = []
+        (0...len).each do |j|
+          tmp_arr << array[j] if (num & (1 << j)) > 0
+        end
+        power_subset_arr << tmp_arr unless power_subset_arr.include?(tmp_arr)
+      end
+      power_subset_arr
+    end
+  end
+end
+
+# To generate Power Subsets
+# require '/Users/ravinayak/Documents/personal_projects/data_structures/backtracking/subsets'
+# subsets = Backtracking::Subsets.new
+# subsets.generate_power_subsets([1, 2, 2])
+# subsets.generate_power_subsets(%w(a b c))
+# subsets.generate_power_subsets(%w(a b c d))
+# # subsets.generate_power_subsets(%w(a a a a))
